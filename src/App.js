818f7fe4ef50f1202ceb6a104446397c2a9dda70
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-05-03 12:34:01 +0100

    added header and footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import './App.css';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-function App() {
-	return <div className="App">Hello</div>;
-}
+const App = () => {
+	return (
+		<>
+			<Header />
+			<main className="py-3">
+				<Container>
+					<h1>Welcome to booze town</h1>
+				</Container>
+			</main>
+			<Footer />
+		</>
+	);
+};
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Nav, Navbar, Container } from 'react-bootstrap';
+
+const Header = () => {
+	return (
+		<header>
+			<Navbar bg="light" expand="lg" collapseOnSelect>
+				<Container>
+					<Navbar.Brand href="#home">Swally</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="mr-auto">
+							<Nav.Link href="/beers">Beers</Nav.Link>
+							<Nav.Link href="/spirits">Spirits</Nav.Link>
+							<Nav.Link href="/wines">Wines</Nav.Link>
+							<Nav.Link href="/whiskey">Whiskey</Nav.Link>
+						</Nav>
+						<Nav className="ml-auto">
+							<Nav.Link href="/cart">
+								<i className="fas fa-shopping-bag"></i>
+							</Nav.Link>
+							<Nav.Link href="/login">Sign In</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</header>
+	);
+};
+
+export default Header;
